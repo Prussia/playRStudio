7fe52072f043ec6c1ec56ddf98d41a83833a5d14
Prussia <prussia.hu@gmail.com>
2019-04-05 17:44:17 +0800

    Update R_CSV.R

--- R_CSV.R
+++ R_CSV.R
@@ -17,3 +17,58 @@ data <- read.csv("input.csv")
 print(is.data.frame(data))
 print(ncol(data))
 print(nrow(data))
+
+
+# Create a data frame.
+data <- read.csv("input.csv")
+
+# Get the max salary from data frame.
+sal <- max(data$salary)
+print(sal)
+
+# Create a data frame.
+data <- read.csv("input.csv")
+
+# Get the max salary from data frame.
+sal <- max(data$salary)
+
+# Get the person detail having max salary.
+retval <- subset(data, salary == max(salary))
+print(retval)
+
+# Create a data frame.
+data <- read.csv("input.csv")
+
+retval <- subset( data, dept == "IT")
+print(retval)
+
+# Create a data frame.
+data <- read.csv("input.csv")
+
+info <- subset(data, salary > 600 & dept == "IT")
+print(info)
+
+# Create a data frame.
+data <- read.csv("input.csv")
+
+retval <- subset(data, as.Date(start_date) > as.Date("2014-01-01"))
+print(retval)
+
+# Create a data frame.
+data <- read.csv("input.csv")
+retval <- subset(data, as.Date(start_date) > as.Date("2014-01-01"))
+
+# Write filtered data into a new file.
+write.csv(retval,"output.csv")
+newdata <- read.csv("output.csv")
+print(newdata)
+
+# Create a data frame.
+data <- read.csv("input.csv")
+retval <- subset(data, as.Date(start_date) > as.Date("2014-01-01"))
+
+# Write filtered data into a new file.
+write.csv(retval,"output.csv", row.names = FALSE)
+newdata <- read.csv("output.csv")
+print(newdata)
+
